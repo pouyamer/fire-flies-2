445c8e58db9c9c46cdd403823750ff92619df3d9
Pouya Moazzezi <pmoazzezi@gmail.com>
2024-09-26 16:45:21 +0330

    [Tweak] values

--- src/core/configs/hue.config.ts
+++ src/core/configs/hue.config.ts
@@ -1,7 +1,18 @@
-import { ChangeType } from "../enums";
+import { ChangeType, ChangingValueMethod } from "../enums";
 import { ChangingValueConfig } from "../types";
 
 export const hueConfig: ChangingValueConfig = {
-  type: ChangeType.NoChange,
-  value: 30
+  type: ChangeType.FlipFlop,
+  value: 0,
+  increment: {
+    min: 0.3,
+    max: .36,
+  },
+  decrement: {
+    min: .4,
+    max: .46
+  },
+  minPossibleValue: 0,
+  startingMethod: ChangingValueMethod.Decrement,
+  maxPossibleValue: 40,
 }
