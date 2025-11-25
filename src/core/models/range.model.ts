0f2e3b0f4db85e65b817539db8097ac874b0eb37
Pouya Moazzezi <pmoazzezi@gmail.com>
2025-11-25 16:22:57 +0330

    [Add] new constructor overload for range

--- src/core/models/range.model.ts
+++ src/core/models/range.model.ts
@@ -2,8 +2,16 @@ export class Range {
   public min: number;
   public max: number;
 
-  constructor(model: Partial<Range> = {}) {
-    this.min = model.min ?? 0;
-    this.max = model.max ?? 0;
+  constructor(min: number, max: number);
+  constructor(model: Partial<Range>);
+  constructor(first: Partial<Range> | number = {}, second?: number) {
+    if(typeof first === 'number') {
+      this.min = first;
+      this.max = second ?? 0;
+    }
+    else {
+      this.min = first.min ?? 0;
+      this.max = first.max ?? 0;
+    }
   }
 }
