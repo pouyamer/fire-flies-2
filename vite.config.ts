6eb913f79856aebb242569621fbb9f95e361db5e
Pouya Moazzezi <pmoazzezi@gmail.com>
2026-04-28 21:51:46 +0330

    [Add] plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   server: {
     port: 5501,
     open: true
-  }
+  },
+  plugins: [
+    checker({
+      typescript: true,
+    })
+  ]
 });
